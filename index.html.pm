a5ea819b55f14a239e58bf76e52853952664ee3d
Beat Hagenlocher <bah@posteo.de>
2024-08-25 16:52:30 +0200

    Add Anmeldungsinfos

--- index.html.pm
+++ index.html.pm
@@ -14,6 +14,9 @@
     ◊par{
       Die Kinder sind nach Alter in ◊inner-link["Teams" "./freizeit.html#teams"] eingeteilt. Innerhalb eines Teams gibt es ◊inner-link["Gruppen" "./freizeit.html#gruppen"], die von ◊inner-link["Gruppenleiter*innen" "./freizeit.html#gruppenleiter-innen"] betreut werden.
     }
+    ◊par{
+      ◊outer-link["Informationsblatt für Eltern" "./files/Erste_Informationen_zu_den_Freizeiten_2024.pdf"]
+    }
   }
   ◊section["Aktuelles"]{
     ◊div[#:class "flex flex-wrap gap-4 items-start"]{
@@ -35,7 +38,7 @@
           Am Samstag, den 24. August findet von 10:00-14:00 das ◊it{Eltern-Kind-Fest} der 2. Freizeit statt.
         }
         ◊par{
-          ◊outer-link["Flyer zum EKF" "https://github.com"]
+          ◊outer-link["Flyer zum EKF (existiert nicht)" ""]
         }
       }
     }
@@ -66,9 +69,39 @@
       }
     }
   }
+  ◊section["Informationen zur Kinderanmeldung"]{
+    ◊par{
+      Die Anmeldung ist nur für einen der beiden Freizeitabschnitte möglich (also entweder für die 1. Freizeit oder für die 2. Freizeit). In jedem Freizeitabschnitt gibt es Plätze für 330 Kinder, wobei die 2. Freizeit in den vergangenen Jahren etwas weniger schnell ausbebucht war.
+    }
+    ◊par{
+      Die Anmeldung zu den Freizeiten ist grundsätzlich ◊bold{nur online} möglich. Ab dem ◊bold{1. Februar} wird der Anmeldelink auf unserer Homepage freigeschaltet. Sollten bis zum 7. Februar mehr Kinder angemeldet sein, als es Plätze hat, wird zwischen allen Kinderanmeldungen vom 1. bis zum 7. Februar per Los entschieden.
+    }
+    ◊par{
+      Die gestaffelten Elternbeiträge für Ihre Anmeldung entnehmen Sie bitte der Beitragstabelle auf dieser Seite weiter unten.
+    }
+    ◊par{
+      Um den Geschwisterkinder-Rabatt zu erhalten, müssen Geschwisterkinder gemeinsam (nicht einzeln) angemeldt werden. Ein Rabatt kann nicht nachträglich gewährt werden. Zudem gilt der Geschwisterrabatt nicht abschnittsübergreifend (sondern jeweils nur für einen Freizeitabschnitt).
+    }
+    ◊par{
+      Die Teilnahmebestätigung erhalten Sie bis Mitte, spätestens aber bis Ende April; die Rechnung erhalten Sie bis Ende Mai, spätestens aber bis Mitte Juni. TN-Bestätigung und Rechnung werden per E-Mail verschickt.
+    }
+    ◊par{
+      Bitte haken sie erst ◊bold{nach dem Ende} des jeweiligen Zeitraumes nach, dass sie noch keine Teilnahmebestätigung/Rechnung erhalten haben. Zusätzlich: Schauen sie zuerst im Spam-Ordner nach, bevor sie uns eine Rückfrage schicken.
+    }
+    ◊par{
+      Die Teilnahme ihres Kindes ◊bold{ist erst nach Zahlungseingang sicher}.
+    }
+    ◊par{
+      Wenn sie ihr Kind wieder abmelden wollen, entstehen ◊bold{Rücktrittskosten}. Diese finden sich in den ◊outer-link["Teilnahmebedingungen" "./files/Teilnahmebedingungen_2-seitig.pdf"].
+    }
+  }
+  ◊section["Kosten"]{
+
+  }
   ◊section["Vergangenes"]{
     ◊subsection["2024"]{
-
+      ◊; Karussell mit Liedern, Videos, Bildern aus den jeweiligen Jahren, pro Jahr (oder pro Freizeit?) eines
+      ◊; z.B. T-Shirt-Bild, Steine, Lieder, Videos
     }
   }
 }
