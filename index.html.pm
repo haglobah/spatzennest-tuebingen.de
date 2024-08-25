bbadf0559e110b2eba030283b8434951b2038c75
Beat Hagenlocher <bah@posteo.de>
2024-08-25 14:24:01 +0200

    Add main page skeleton

--- index.html.pm
+++ index.html.pm
@@ -6,16 +6,63 @@
       Willkommen im Spatzennest Tübingen!
     }
     ◊par{
-      Hier werden Sommererinnerungen geschaffen und Freundschaften geknüpft. Ob in der ◊inner-link["Halle" "./freizeit.html#halle"], im ◊inner-link["Werkbereich" "./freizeit.html#werkbereich"], in den ◊inner-link["Teamdörfern" "./freizeit.html#teamdorf"] oder in der ◊inner-link["Schlucht" "freizeit.html#schlucht"] – das Spatzennest bietet eine sichere, spaßerfüllte Umgebung, in der Kinder zwischen 5 und 16 Jahren die Natur erkunden, neue Talente entdecken und an Selbstvertrauen gewinnen können. Seid dabei für einen unvergesslichen Sommer voller Abenteuer!
+      Hier werden Sommererinnerungen geschaffen und Freundschaften geknüpft. Ob in der ◊inner-link["Halle" "./freizeit.html#halle"], im ◊inner-link["Werkbereich" "./freizeit.html#werkbereich"], in den ◊inner-link["Teamdörfern" "./freizeit.html#teamdorf"] oder in der ◊inner-link["Schlucht" "freizeit.html#schlucht"] – das Spatzennest bietet ein Umfeld, in der Kinder zwischen 5 und 16 Jahren die Natur erkunden, neue Talente entdecken und an Selbstvertrauen gewinnen können. Seid dabei für einen unvergesslichen Sommer voller Abenteuer!
+    }
+    ◊par{
+      Das Spatzennest findet jedes Jahr in zwei Freizeiten statt, wobei sich die ◊it{erste Freizeit} über die ersten drei Wochen der Sommerferien und die ◊it{zweite Freizeit} über die letzten drei Wochen der Sommerferien erstreckt.
+    }
+    ◊par{
+      Die Kinder sind nach Alter in ◊inner-link["Teams" "./freizeit.html#teams"] eingeteilt. Innerhalb eines Teams gibt es ◊inner-link["Gruppen" "./freizeit.html#gruppen"], die von ◊inner-link["Gruppenleiter*innen" "./freizeit.html#gruppenleiter-innen"] betreut werden.
     }
   }
-  ◊section["Kosten"]{
-
+  ◊section["Aktuelles"]{
+    ◊div[#:class "flex flex-wrap gap-4 items-start"]{
+      ◊card[#:title "Bussperrung"]{
+        ◊par{
+          Durch die Sperrung in Pfrondorf kann der ◊bold{Igelbus} die Haltestellen ◊bold{Pfrondorf Schönbuchhalle} und ◊bold{Pfrondorf Rathaus} bis auf Weiteres nicht anfahren.
+          Ersatzhaltestelle ist die Haltestelle ◊bold{Lindenstraße}:
+        }
+        ◊par{
+          Hinfahrt 8:47 ◊br{}
+          Rückfahrt 18:06
+        }
+        ◊par{
+          Die anderen Haltestellen werden nach Plan angefahren.
+        }
+      }
+      ◊card[#:title "EKF 2.Freizeit 2024"]{
+        ◊par{
+          Am Samstag, den 24. August findet von 10:00-14:00 das ◊it{Eltern-Kind-Fest} der 2. Freizeit statt.
+        }
+        ◊par{
+          ◊outer-link["Flyer zum EKF" "https://github.com"]
+        }
+      }
+    }
   }
-  ◊section["Gallerie"]{
-
+  ◊section["1.Freizeit 2024 | 29.07.24-15.08.24"]{
+    ◊div[#:class "flex flex-wrap gap-4 items-start"]{
+      ◊card[#:title "Anmeldung Kinder"]{
+      }
+      ◊card[#:title "Bewerbung Gruppenleiter*innen"]{
+      }
+      ◊card[#:title "Bewerbung Küchenmitarbeiter*innen"]{
+      }
+    }
   }
-  ◊section["Aus vergangenen Freizeiten"]{
+  ◊section["2.Freizeit 2024 | 19.08.24-05.09.24"]{
+    ◊div[#:class "flex flex-wrap gap-4 items-start"]{
+      ◊card[#:title "Anmeldung Kinder"]{
+      }
+      ◊card[#:title "Bewerbung Gruppenleiter*innen"]{
+      }
+      ◊card[#:title "Bewerbung Küchenmitarbeiter*innen"]{
+      }
+    }
+  }
+  ◊section["Vergangenes"]{
+    ◊subsection["2024"]{
 
+    }
   }
 }
