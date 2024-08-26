5aa037e7586f132888dffbf1b6f1dd2f193f9acb
Beat Hagenlocher <bah@posteo.de>
2024-08-27 00:16:19 +0200

    Add song badges

--- pollen.rkt
+++ pollen.rkt
@@ -4,15 +4,15 @@
 
 (provide (all-defined-out))
 
-(define (inner-link text url) 
+(define (inner-link url . content) 
   `(a ([href ,url] [rel "noreferrer noopener"]
        [class "text-[#44567e]/80 hover:underline decoration-2 underline-offset-2 decoration-[#44567e]/50 hover:text-[#44567e]"])
-      ,text))
+      ,@content))
 
-(define (outer-link text url) 
+(define (outer-link url . content) 
   `(a ([href ,url] [rel "noreferrer noopener"] [target "_blank"]
        [class "text-[#44567e]/80 hover:underline decoration-2 underline-offset-2 decoration-[#44567e]/50 hover:text-[#44567e]"])
-      ,text))
+      ,@content))
 
 (define (heading-to-id heading)
   (string-downcase
@@ -69,3 +69,10 @@
               (filter (λ (data) (not (equal? data "\n")))
                       (string-split row ",")))))
       rows)))
+
+(define (song . text)
+  `(div ([class "mr-1.5 mb-2"])
+		(div ([class ,"flex items-center rounded-full bg-[#44567e]/10 hover:bg-[#44567e]/40 px-4 py-2 text-sm font-medium leading-5 text-[#44567e]"])
+         (svg ([xmlns "http://www.w3.org/2000/svg"] [fill "none"] [viewBox "0 0 24 24"] [stroke-width "1.5"] [stroke "currentColor"] [class "mx-1 size-5"])
+            (path ([stroke-linecap "round"] [stroke-linejoin "round"] [d "m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z"])))
+         ,@text)))
