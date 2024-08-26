1853012a45398290d5b12ad2d11cbdba763298e0
Beat Hagenlocher <bah@posteo.de>
2024-08-27 00:16:38 +0200

    Add songs (not yet to the code, tough)

--- index.html.pm
+++ index.html.pm
@@ -6,16 +6,16 @@
       Willkommen im Spatzennest Tübingen!
     }
     ◊par{
-      Hier werden Sommererinnerungen geschaffen und Freundschaften geknüpft. Ob in der ◊inner-link["Halle" "./freizeit.html#halle"], im ◊inner-link["Werkbereich" "./freizeit.html#werkbereich"], in den ◊inner-link["Teamdörfern" "./freizeit.html#teamdorf"] oder in der ◊inner-link["Schlucht" "freizeit.html#schlucht"] – das Spatzennest bietet ein Umfeld, in der Kinder zwischen 5 und 16 Jahren die Natur erkunden, neue Talente entdecken und an Selbstvertrauen gewinnen können. Seid dabei für einen unvergesslichen Sommer voller Abenteuer!
+      Hier werden Sommererinnerungen geschaffen und Freundschaften geknüpft. Ob in der ◊inner-link["./freizeit.html#halle"]{Halle}, im ◊inner-link["./freizeit.html#werkbereich"]{Werkbereich}, in den ◊inner-link["./freizeit.html#teamdorf"]{Teamdörfern} oder in der ◊inner-link["freizeit.html#schlucht"]{Schlucht} – das Spatzennest bietet ein Umfeld, in der Kinder zwischen 5 und 16 Jahren die Natur erkunden, neue Talente entdecken und an Selbstvertrauen gewinnen können. Seid dabei für einen unvergesslichen Sommer voller Abenteuer!
     }
     ◊par{
       Das Spatzennest findet jedes Jahr in zwei Freizeiten statt, wobei sich die ◊it{erste Freizeit} über die ersten drei Wochen der Sommerferien und die ◊it{zweite Freizeit} über die letzten drei Wochen der Sommerferien erstreckt.
     }
     ◊par{
-      Die Kinder sind nach Alter in ◊inner-link["Teams" "./freizeit.html#teams"] eingeteilt. Innerhalb eines Teams gibt es ◊inner-link["Gruppen" "./freizeit.html#gruppen"], die von ◊inner-link["Gruppenleiter*innen" "./freizeit.html#gruppenleiter-innen"] betreut werden.
+      Die Kinder sind nach Alter in ◊inner-link["./freizeit.html#teams"]{Teams} eingeteilt. Innerhalb eines Teams gibt es ◊inner-link["./freizeit.html#gruppen"]{Gruppen}, die von ◊inner-link["./freizeit.html#gruppenleiter-innen"]{Gruppenleiter*innen} betreut werden.
     }
     ◊par{
-      ◊outer-link["Informationsblatt für Eltern" "./files/Erste_Informationen_zu_den_Freizeiten_2024.pdf"]
+      ◊outer-link["./files/Erste_Informationen_zu_den_Freizeiten_2024.pdf"]{Informationsblatt für Eltern}
     }
   }
   ◊section["Aktuelles"]{
@@ -38,14 +38,14 @@
           Am Samstag, den 24. August findet von 10:00-14:00 das ◊it{Eltern-Kind-Fest} der 2. Freizeit statt.
         }
         ◊par{
-          ◊outer-link["Flyer zum EKF (existiert nicht)" ""]
+          ◊outer-link[""]{Flyer zum EKF (existiert nicht)}
         }
       }
     }
   }
   ◊section["1.Freizeit 2024 | 29.07.24-15.08.24"]{
     ◊par{
-      ◊outer-link["Elternbrief zur 1. Freizeit" "./files/Eltern-Kind-Brief_1._FZ_2024.pdf"]
+      ◊outer-link["./files/Eltern-Kind-Brief_1._FZ_2024.pdf"]{Elternbrief zur 1. Freizeit}
     }
     ◊div[#:class "flex flex-wrap gap-4 items-start"]{
       ◊card[#:title "Anmeldung Kinder"]{
@@ -58,7 +58,7 @@
   }
   ◊section["2.Freizeit 2024 | 19.08.24-05.09.24"]{
     ◊par{
-      ◊outer-link["Elternbrief zur 2.Freizeit" "./files/Eltern-Kind-Brief_2._FZ_2024.pdf"]
+      ◊outer-link["./files/Eltern-Kind-Brief_2._FZ_2024.pdf"]{Elternbrief zur 2.Freizeit}
     }
     ◊div[#:class "flex flex-wrap gap-4 items-start"]{
       ◊card[#:title "Anmeldung Kinder"]{
@@ -93,7 +93,7 @@
       Die Teilnahme ihres Kindes ◊bold{ist erst nach Zahlungseingang sicher}.
     }
     ◊par{
-      Wenn sie ihr Kind wieder abmelden wollen, entstehen ◊bold{Rücktrittskosten}. Diese finden sich in den ◊outer-link["Teilnahmebedingungen" "./files/Teilnahmebedingungen_2-seitig.pdf"].
+      Wenn sie ihr Kind wieder abmelden wollen, entstehen ◊bold{Rücktrittskosten}. Diese finden sich in den ◊outer-link["./files/Teilnahmebedingungen_2-seitig.pdf"]{Teilnahmebedingungen}.
     }
   }
   ◊section["Kosten"]{
@@ -103,8 +103,8 @@
     ◊par{
       Die offiziellen Dokumente dazu finden sie hier:
       ◊ul[#:class "list-disc list-inside"]{
-        ◊li{◊outer-link["Erläuterung pauschalisiertes Netto-Jahreseinkommen | Stadt Tübingen" "https://www.tuebingen.de/verwaltung/uploads/erlaeuterung_jahresnettoeinkommen_schulkind.pdf"]}
-        ◊li{◊outer-link["Beispielberechnung zum pauschalisierten Netto-Jahreseinkommen | Stadt Tübingen" "https://www.tuebingen.de/verwaltung/uploads/beispiele_pausch_jahresnetto.pdf"]}
+        ◊li{◊outer-link["https://www.tuebingen.de/verwaltung/uploads/erlaeuterung_jahresnettoeinkommen_schulkind.pdf"]{Erläuterung pauschalisiertes Netto-Jahreseinkommen | Stadt Tübingen}}
+        ◊li{◊outer-link["https://www.tuebingen.de/verwaltung/uploads/beispiele_pausch_jahresnetto.pdf"]{Beispielberechnung zum pauschalisierten Netto-Jahreseinkommen | Stadt Tübingen}}
       }
     }
     ◊par{
@@ -142,20 +142,35 @@
     }
     ◊ol[#:class "ml-6 list-outside list-decimal"]{
       ◊li{
-        Eine Ratenzahlung mit zwei Teilzahlungen ist möglich. Sollten Sie diese nutzen wollen, ◊inner-link["kontaktieren sie uns bitte" "./index.html#kontakt"].
+        Eine Ratenzahlung mit zwei Teilzahlungen ist möglich. Sollten Sie diese nutzen wollen, ◊inner-link["./index.html#kontakt"]{kontaktieren sie uns bitte}.
       }
       ◊li{
         Sollten Sie eine KreisBonusCard Junior besitzen, erhalten sie eine Ermäßigung von 50% auf den Elternbeitrag.
       }
       ◊li{
-        Sollten Sie auch mit Ermäßigung nicht in der Lage sein, die Gebühren zu bezahlen (oder keine KreisBonusCard Junior besitzen), ◊inner-link["kontaktieren sie uns bitte" "./index.html#kontakt"]. Und: Nutzen Sie diese Möglichkeit – die Teilnahme am Spatzennest darf nicht an zu hohen Gebühren scheitern.
+        Sollten Sie auch mit Ermäßigung nicht in der Lage sein, die Gebühren zu bezahlen (oder keine KreisBonusCard Junior besitzen), ◊inner-link["./index.html#kontakt"]{kontaktieren sie uns bitte}. Und: Nutzen Sie diese Möglichkeit – die Teilnahme am Spatzennest darf nicht an zu hohen Gebühren scheitern.
       }
     }
   }
   ◊section["Vergangenes"]{
+    ◊; Karussell mit Liedern, Videos, Bildern aus den jeweiligen Jahren, pro Jahr (oder pro Freizeit?) eines
+    ◊; z.B. T-Shirt-Bild, Steine, Lieder, Videos
     ◊subsection["2024"]{
-      ◊; Karussell mit Liedern, Videos, Bildern aus den jeweiligen Jahren, pro Jahr (oder pro Freizeit?) eines
-      ◊; z.B. T-Shirt-Bild, Steine, Lieder, Videos
+      ◊div[#:class ""]{
+        ◊outer-link["./files/lieder/.pdf"]{}
+      }
+    }
+    ◊subsection["2023"]{
+      ◊div[#:class "inline-flex gap-2"]{
+        ◊outer-link["./files/lieder/Teamlied_2023-1_Team1_TheDark_v3-1.pdf"]{◊song{The Dark}}
+        ◊outer-link["./files/lieder/Teamlied-Zugeheim-2023IT2.pdf"]{◊song{Zugeheim}}
+      }
+    }
+    ◊subsection["2022"]{
+      ◊div[#:class "inline-flex gap-2"]{
+        ◊outer-link["./files/lieder/Teamlied_2023-1_Team1_TheDark_v3-1.pdf"]{◊song{The Dark}}
+        ◊outer-link["./files/lieder/Teamlied-Zugeheim-2023IT2.pdf"]{◊song{Zugeheim}}
+      }
     }
   }
 }
